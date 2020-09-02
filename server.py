4f8565d4de2d7b710b84598c5a150f50cbdc5346
Eric Zou <cheetsheatoverlode@gmail.com>
2020-09-02 15:03:47 -0400

    Fixed another python exploit bug

--- server.py
+++ server.py
@@ -80,7 +80,7 @@ class Utility(commands.Cog):
         brief="Run Python commands!"
     )
     async def python(self, ctx, *, code):
-        if ("input(" not in str(code)):
+        if ("input(" not in str(code) and "&&" not in str(code) and "||" not in str(code)):
             try:
                 compile(code, 'script', 'eval')
                 os.system(f"echo {code} > shell.py")
