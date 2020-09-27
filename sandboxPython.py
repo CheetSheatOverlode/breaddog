1239556fef9b6e120dfc7ec62ec6a18d4d6348a9
Eric Zou <cheetsheatoverlode@gmail.com>
2020-09-27 15:20:12 -0400

    Added docstring, added empty function

---
+++ sandboxPython.py
@@ -0,0 +1,9 @@
+"""
+Sandbox Python
+A version of Python (implemented in Python) that can be used for running untrusted code.
+Open source, feel free to use for your own purposes, but please give credit.
+"""
+
+
+def run(code):
+    pass
